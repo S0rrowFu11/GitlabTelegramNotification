cf392c009b8ab72e8c117953183f8b46d51d4ef7
Vasily Sviridov <vasily-sviridov@eduprog-team.ru>
2023-02-02 16:40:45 +0300

    update

--- app/service/send_message.py
+++ app/service/send_message.py
@@ -3,11 +3,15 @@ import requests
 import os
 from dotenv import load_dotenv
 from app.utils.config import env_path, bot_secret
+from loguru import logger
 
 
 async def send_message(message_text: str, ids: list[int]):
-    load_dotenv(env_path())
-    token = os.environ.get(bot_secret())
-    for id in ids:
-        requests.post(f"https://api.telegram.org/bot{token}/sendMessage", data={
-                      "chat_id": id, "text": message_text, "parse_mode": "Markdown", "disable_web_page_preview": "true"})
+    try:
+        load_dotenv(env_path())
+        token = os.environ.get(bot_secret())
+        for id in ids:
+            requests.post(f"https://api.telegram.org/bot{token}/sendMessage", data={
+                "chat_id": id, "text": message_text, "parse_mode": "Markdown", "disable_web_page_preview": "true"})
+    except requests.exceptions.HTTPError as err:
+        logger.error(err)
